e10643e41db965771cb91e32c1361a762c3d4edd
Viktorija Goferienė <v.goferiene@gmail.com>
2023-03-29 23:40:05 +0300

    Add 01_uzduotis

---
+++ 01_uzduotis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace _01_uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Labas, Viktorija");
+            int amzius = 36;
+            Console.WriteLine($"Įvestas amžius: {amzius}");
+            Console.WriteLine();
+            int skaicius = 25;
+            //užduotis su skaičiu 25 be tarpų
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.WriteLine();
+            //užduotis su skaičiu 25 su tarpais
+            Console.Write(skaicius + " ");
+            Console.Write(skaicius + " ");
+            Console.Write(skaicius + " ");
+            Console.Write(skaicius + " ");
+            Console.Write(skaicius + " ");
+            Console.WriteLine();
+        }
+    }
+}
